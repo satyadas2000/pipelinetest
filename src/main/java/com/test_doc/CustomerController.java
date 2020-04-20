bcd219477fab4c19c99c7481102abb82fbd8a844
satyadas2000 <satyadas20000@gmail.com>
2020-04-20 11:41:35 +0530

    dsd

---
+++ src/main/java/com/test_doc/CustomerController.java
@@ -0,0 +1,21 @@
+package com.test_doc;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+ 
+ 
+@RestController
+@RequestMapping("/test-doc")
+public class CustomerController
+{
+    @RequestMapping("/customers")
+    public List<Customer> findAll()
+    {
+      List<Customer> customerList = new ArrayList<Customer>();
+      customerList.add(new Customer(1, "frank"));
+      customerList.add(new Customer(2, "john"));
+      return customerList;
+    }
+}
